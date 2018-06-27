dc4c6fefd6731f19af62bfa477f47b3c0d56f207
Ashley Fernandes <ashleefernandes@gmail.com>
2018-06-27 20:46:42 +0530

    added new project : gradeBook

---
+++ GradeBook/GradeBook/GradeBookClass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class GradeBookClass
+    {
+        public void AddGrade(float grade)
+        {
+            grades.Add(grade);
+        }
+
+        List<float> grades = new List<float>();
+    }
+}
---
+++ GradeBook/GradeBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBookClass book = new GradeBookClass();
+            book.AddGrade(91);
+            book.AddGrade(34.2f);
+            Console.ReadLine();
+        }
+    }
+}
